309d283778acd853a2180477bf0aa09387817645
Youssef Wagieh <joeyoggie@gmail.com>
2018-11-12 19:03:12 +0200

    Retry the upload process multiple times to make sure it succeeds, before telling the user the operation failed

--- app/src/main/java/com/ronixtech/ronixhome/fragments/UpdateDeviceFirmwareUploadFragment.java
+++ app/src/main/java/com/ronixtech/ronixhome/fragments/UpdateDeviceFirmwareUploadFragment.java
@@ -345,94 +345,111 @@ public class UpdateDeviceFirmwareUploadFragment extends Fragment {
         @Override
         protected Void doInBackground(Void... params) {
             HttpURLConnection conn = null;
-            DataOutputStream dos = null;
-            String lineEnd = "\r\n";
-            String twoHyphens = "--";
-            String boundary = "*****";
-            int bytesRead, bytesAvailable, bufferSize;
-            byte[] buffer;
-            int maxBufferSize = 1 * 1024 * 1024;
+            statusCode = 0;
+            int numberOfRetries = 0;
+            while (statusCode != 200 && numberOfRetries <= Device.CONFIG_NUMBER_OF_RETRIES){
+                DataOutputStream dos = null;
+                FileInputStream fileInputStream = null;
+                InputStream in = null;
+                BufferedReader bufferedReader = null;
+                String lineEnd = "\r\n";
+                String twoHyphens = "--";
+                String boundary = "*****";
+                int bytesRead, bytesAvailable, bufferSize;
+                byte[] buffer;
+                int maxBufferSize = 1 * 1024 * 1024;
 
-            try {
-                String upLoadServerUri = "http://" + device.getIpAddress() /*+ ":88"*/ + Constants.DEVICE_UPLOAD_FIRMWARE_URL;
-                Log.d(TAG, "uploadFirmware URL: " + upLoadServerUri);
+                try {
+                    String upLoadServerUri = "http://" + device.getIpAddress() /*+ ":88"*/ + Constants.DEVICE_UPLOAD_FIRMWARE_URL;
+                    Log.d(TAG, "uploadFirmware URL: " + upLoadServerUri);
 
-                // open a URL connection to the Servlet
-                FileInputStream fileInputStream = activity.openFileInput(filename);
-                URL url = new URL(upLoadServerUri);
+                    // open a URL connection to the Servlet
+                    fileInputStream = activity.openFileInput(filename);
+                    URL url = new URL(upLoadServerUri);
 
-                // Open a HTTP connection to the URL
-                conn = (HttpURLConnection) url.openConnection();
-                conn.setDoInput(true); // Allow Inputs
-                conn.setDoOutput(true); // Allow Outputs
-                conn.setUseCaches(false); // Don't use a Cached Copy
-                conn.setRequestMethod("POST");
-                conn.setRequestProperty("Connection", "Keep-Alive");
-                //conn.setRequestProperty("ENCTYPE", "multipart/form-data");
-                //conn.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
-                //conn.setRequestProperty(filename, filename);
+                    // Open a HTTP connection to the URL
+                    conn = (HttpURLConnection) url.openConnection();
+                    conn.setDoInput(true); // Allow Inputs
+                    conn.setDoOutput(true); // Allow Outputs
+                    conn.setUseCaches(false); // Don't use a Cached Copy
+                    conn.setRequestMethod("POST");
+                    conn.setRequestProperty("Connection", "Keep-Alive");
+                    //conn.setRequestProperty("ENCTYPE", "multipart/form-data");
+                    //conn.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
+                    //conn.setRequestProperty(filename, filename);
 
-                dos = new DataOutputStream(conn.getOutputStream());
+                    dos = new DataOutputStream(conn.getOutputStream());
 
-                //dos.writeBytes(twoHyphens + boundary + lineEnd);
-                //dos.writeBytes("Content-Disposition: form-data; name=\""+filename+"\";filename=\"" + filename + "\"" + lineEnd);
+                    //dos.writeBytes(twoHyphens + boundary + lineEnd);
+                    //dos.writeBytes("Content-Disposition: form-data; name=\""+filename+"\";filename=\"" + filename + "\"" + lineEnd);
 
-                //dos.writeBytes(lineEnd);
+                    //dos.writeBytes(lineEnd);
 
-                // create a buffer of maximum size
-                bytesAvailable = fileInputStream.available();
-
-                bufferSize = Math.min(bytesAvailable, maxBufferSize);
-                buffer = new byte[bufferSize];
-
-                // read file and write it into form...
-                bytesRead = fileInputStream.read(buffer, 0, bufferSize);
-
-                while (bytesRead > 0) {
-                    dos.write(buffer, 0, bufferSize);
+                    // create a buffer of maximum size
                     bytesAvailable = fileInputStream.available();
+
                     bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                    buffer = new byte[bufferSize];
+
+                    // read file and write it into form...
                     bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+
+                    while (bytesRead > 0) {
+                        dos.write(buffer, 0, bufferSize);
+                        bytesAvailable = fileInputStream.available();
+                        bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                        bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+                    }
+
+                    // send multipart form data necesssary after file
+                    // data...
+                    //dos.writeBytes(lineEnd);
+                    //dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+
+                    in = new BufferedInputStream(conn.getInputStream());
+                    bufferedReader = new BufferedReader(new InputStreamReader(in));
+                    StringBuilder result = new StringBuilder();
+                    String dataLine;
+                    while((dataLine = bufferedReader.readLine()) != null) {
+                        result.append(dataLine);
+                    }
+
+                    Log.d(TAG, "uploadFirmware response: " + result.toString());
+
+                    // Responses from the server (code and message)
+                    statusCode = conn.getResponseCode();
+
+                    Log.d(TAG, "uploadFirmware response code: " + statusCode);
+
+                    // close the streams //
+                    fileInputStream.close();
+                    dos.flush();
+                    dos.close();
+                    in.close();
+                    bufferedReader.close();
+                } catch (Exception e) {
+                    Log.d(TAG, "Exception: " + e.getMessage());
+                }finally {
+                    try {
+                        if (dos != null) {
+                            dos.flush();
+                            dos.close();
+                        }
+                        if (in != null)
+                            in.close();
+                        if (fileInputStream != null)
+                            fileInputStream.close();
+                        if (bufferedReader != null)
+                            bufferedReader.close();
+                    } catch (IOException ignored) {
+
+                    }
+                    if(conn != null) {
+                        conn.disconnect();
+                    }
+                    numberOfRetries++;
                 }
-
-                // send multipart form data necesssary after file
-                // data...
-                //dos.writeBytes(lineEnd);
-                //dos.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
-
-                InputStream in = new BufferedInputStream(conn.getInputStream());
-                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in));
-                StringBuilder result = new StringBuilder();
-                String dataLine;
-                while((dataLine = bufferedReader.readLine()) != null) {
-                    result.append(dataLine);
-                }
-
-                Log.d(TAG, "uploadFirmware response: " + result.toString());
-
-                // Responses from the server (code and message)
-                statusCode = conn.getResponseCode();
-
-                Log.d(TAG, "uploadFirmware response code: " + statusCode);
-
-                if (statusCode == 200) {
-                    // messageText.setText(msg);
-                    //Toast.makeText(ctx, "File Upload Complete.",
-                    //      Toast.LENGTH_SHORT).show();
-                    // recursiveDelete(mDirectory1);
-                }
-                // close the streams //
-                fileInputStream.close();
-                dos.flush();
-                dos.close();
-                in.close();
-                bufferedReader.close();
-
-            } catch (Exception e) {
-                // dialog.dismiss();
-                e.printStackTrace();
             }
-            // dialog.dismiss();
 
             return null;
         }
